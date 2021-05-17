475a789634409a4cdbb529aab5397eeeeb3c95fb
Ale_lata <nazotrol@gmail.com>
2021-05-17 11:35:19 +0900

    se agrega una variable magica a metodo

--- millas_kilometros.py
+++ millas_kilometros.py
@@ -1,16 +1,19 @@
-def convertidor (unidades, distancia):
-    if unidades == 0:
-        distancia_0 = distancia* 1.609344 
-        print(f'La distancia en kilometros es : {distancia_0}')
-        return distancia_0
-    else:
-        distancia_1 = distancia* 0.62137119 
-        print(f'La distancia en millas es : {distancia_1}')
-        return distancia_1
+FACTOR_CONVERSION_KM = 1.609344
+FACTOR_CONVERSION_MILLAS = 0.62137119
 
+def convertidor (distancia,constante) -> float:
+    return distancia*constante
 
 if __name__=='__main__':
     unidades = int(input(f'¿Comvertir distancia de: (0) millas a kilometros o (1) kilometros a millas? : '))
+    
     distancia = float(input(f'¿Cual es la distancia?: '))
-    distancia_final = convertidor(unidades, distancia)
-    print(f'la distancia final es: {distancia_final}')
+    
+    if unidades == 0:
+        distancia_0 = convertidor(distancia, FACTOR_CONVERSION_KM) 
+        print(f'La distancia en kilometros es : {distancia_0}')
+    elif unidades == 1:
+        distancia_1 =convertidor(distancia, FACTOR_CONVERSION_MILLAS)
+        print(f'La distancia en millas es : {distancia_1}')
+    else:
+        raise NameError('Elige una opcion valida: 0  o 1 ')
